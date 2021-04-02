5b36204749f83d64afd093ba99550e4a54e89aed
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-02 09:52:28 +0100

    exercise 2.6 commit

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,17 +6,47 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+      id: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const changeName = (e) => {
+    setNewName(e.target.value);
+  }
+
+  const Person = ({persons}) => {
+    return (
+      persons.map( person =>
+        <li style={{listStyle: 'none'}}
+        key={person.name}> {person.name}</li>
+      ))};
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-      <div>debug: {newName}</div>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={changeName} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        <ul style={{paddingInlineStart: 0}}>
+          <Person persons={persons} />
+        </ul>
+      </div>
     </div>
   )
 }

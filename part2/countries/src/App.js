617a63b58533a3ea05cfe2ce1352df1dc8b089f7
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-07 22:12:46 +0100

    clean up code, folder, add dependencies and base functions

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [newCountry, setNewCountry] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response)
+      })
+  }, [])
+  
+  const result = countries.data['name'].filter(country => country.toLowerCase().includes(newCountry.toLowerCase()))
+  console.log(result);
+
+  // console.log(countries);
+
+  const handleChange = (e) => setNewCountry(e.target.value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      find countries
+      <form /*onSubmit={addCountry}*/ style={{display: 'inline-block'}}>
+        <input value={newCountry}
+        onChange={handleChange}/>
+      </form>
     </div>
   );
 }

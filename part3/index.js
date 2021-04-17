659858b1bf2ec12bf3dd46937ebd20c0687c04fd
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-17 11:33:46 +0100

    exercise 3.15 commit

--- part3/index.js
+++ part3/index.js
@@ -38,11 +38,13 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+});
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -73,6 +75,19 @@ app.post('/api/persons', (request, response) => {
     })
 });
 
+//ERROR HANDLER//
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted ID'})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
+
+//PORT//
 PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

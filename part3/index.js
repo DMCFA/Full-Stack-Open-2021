72a1bbd76773a8974a11ab4d894486373c8bcc4c
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-17 10:40:53 +0100

    exercise 3.14 commit

--- part3/index.js
+++ part3/index.js
@@ -15,34 +15,13 @@ morgan.token('body', (request, response) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms  :body'));
 
 
-persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456'
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523'
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345'
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendick',
-        number: '39-23-6423122'
-    }
-]
-
-const newId = () => Math.floor(Math.random() * 1000)
-
 app.get('/api/persons/', (request, response) => {
-    response.json(persons)
-})
+    Person.find({})
+    .then(persons => {
+        response.json(persons)
+    })
+});
+
 
 app.get('/info/', (request, response) => {
     const entries = persons.length
@@ -53,14 +32,10 @@ app.get('/info/', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+    .then(persons => {
+        response.json(persons)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -71,35 +46,34 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    const personName = persons.map(person => person.name)
+//     const personName = persons.map(person => person.name)
 
-    if(!body.name) {
-        return response.status(400).json({
-            error: 'name is missing'
-        })
-    } else if (!body.number) {
-        return response.status(400).json({
-            error: 'number is missing'
-        })
-    } else if (personName.includes(body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-    })
-}
+//     if(!body.name) {
+//         return response.status(400).json({
+//             error: 'name is missing'
+//         })
+//     } else if (!body.number) {
+//         return response.status(400).json({
+//             error: 'number is missing'
+//         })
+//     } else if (personName.includes(body.name)) {
+//         return response.status(400).json({
+//             error: 'name must be unique'
+//     })
+// }
 
-    const person = {
-        id: newId(),
+    const person = new Person ({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
+    person.save()
+    .then(savedPerson => {
+        response.json(savedPerson)
+    })
+});
 
-    response.json(person)
-
-})
-
-PORT = process.env.PORT || 3001
+PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })

d0cdea54b322282148b76d9f36a7c47ebb5313e1
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-19 09:38:02 +0100

    exercise 3.19 commit

--- part3/models/person.js
+++ part3/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url);
@@ -12,9 +13,15 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindA
 });
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
     number: String,
   })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {

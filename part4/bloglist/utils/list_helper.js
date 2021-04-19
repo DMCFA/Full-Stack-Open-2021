f7f4c687dee4bbb8fb953afbaa700003b36a21e3
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-19 19:09:13 +0100

    exercise 4.4 commit

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.reduce((sum, curr) => {
+        return sum + curr.likes
+    }, 0)
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }

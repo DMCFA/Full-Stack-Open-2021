95175209c369dec5e68a246e0392f0f5bb81f6de
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-20 22:07:54 +0100

    exercise 4.8 commit

---
+++ part4/bloglist/tests/bloglist.test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let blog of helper.blogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
+
+test('get all blogs in json format', async () => {
+    await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    const res = await api.get('/api/blogs')
+
+    expect(res.body).toHaveLength(helper.blogs.length)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})

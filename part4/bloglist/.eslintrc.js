b3cbec14bcddc1e9641a3cf750c7cd2ef6904dc0
Duarte Almeida <duartemalmeida@gmail.com>
2021-04-22 16:58:37 +0100

    exercise 4.15 commit

--- part4/bloglist/.eslintrc.js
+++ part4/bloglist/.eslintrc.js
@@ -1,35 +1,32 @@
 /* eslint-disable linebreak-style */
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es2021': true,
-    'node': true,
-    'jest': true
-  },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12
-  },
-  'rules': {
-    'indent': [
-      'error',
-      tab
-    ],
-    'linebreak-style': [
-      'error',
-      'windows'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'no-console': 0,
-  },
-  "jest": {
-    "testEnvironment": "node"
-  }
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+        'jest': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-console': 0,
+    },
 }

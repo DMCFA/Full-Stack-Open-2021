507a51a93e9f3d4f5ddb2342cb63463934b1dade
Duarte Almeida <duartemalmeida@gmail.com>
2021-05-13 19:03:20 +0100

    exercise 6.19 commit

--- part6/redux-anecdotes-backend/src/components/Notification.js
+++ part6/redux-anecdotes-backend/src/components/Notification.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
-  if (!notification) return null
+const Notification = (props) => {
+  if (!props.notification) return null
   const style = {
     border: 'solid',
     padding: 10,
@@ -11,9 +10,17 @@ const Notification = () => {
   }
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const connectedNotification = connect(mapStateToProps)(Notification)
+
+export default connectedNotification

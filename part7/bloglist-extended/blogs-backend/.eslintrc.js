304678b0a231a6ae5ffb09aef4ee02575bf9a9df
Duarte Almeida <duartemalmeida@gmail.com>
2021-05-20 15:04:02 +0100

    blog part7 initial commit - boiler plate taken from solutions

---
+++ part7/bloglist-extended/blogs-backend/.eslintrc.js
@@ -0,0 +1,32 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+        'jest': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-console': 0,
+    },
+}
